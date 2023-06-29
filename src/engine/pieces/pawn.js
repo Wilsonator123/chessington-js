10dc8cf3f3979f458cd6ef92bcfab8d3ee1301af
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-06-29 15:18:53 +0100

    Red-1 and Red-2

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,20 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let currentSquare = board.findPiece(this);
+        if(this.player === Player.WHITE){
+            let availableMoves = [new Square(currentSquare.row+1, currentSquare.col)];
+            if(currentSquare.row === 1){
+                availableMoves.push(new Square(currentSquare.row + 2, currentSquare.col));
+            }
+            return availableMoves;
+        }
+        else {
+            let availableMoves = [new Square(currentSquare.row-1, currentSquare.col)];
+            if(currentSquare.row === 6){
+                availableMoves.push(new Square(currentSquare.row - 2, currentSquare.col));
+            }
+            return availableMoves;
+        }
     }
 }
