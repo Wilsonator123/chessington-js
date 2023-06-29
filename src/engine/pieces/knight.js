e645448b58d5f726d1f4a20e9d1d51af6d9d7cbc
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-06-29 16:50:15 +0100

    Red-6 - Knight Movement

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Player from "../player";
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +8,30 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const directions = [
+            {dr: -2, dc: -1}, // Up->Left
+            {dr: -2, dc: 1}, // Up->Right
+            {dr: -1, dc: -2}, // Left->Up
+            {dr: 1, dc: -2}, // Left->Down
+            {dr: 2, dc: -1}, // Down->Left
+            {dr: 2, dc: 1},  // Down->Right
+            {dr: -1, dc: 2},  // Right->Up
+            {dr: 1, dc: 2}    // Right->Down
+        ];
+
+        let availableMoves = [];
+        let currentSquare = board.findPiece(this);
+
+        for(let direction of directions){
+            let currentRow = currentSquare.row+direction.dr;
+            let currentCol = currentSquare.col+direction.dc;
+
+            if(0<=currentRow && currentRow<8 && 0<=currentCol && currentCol<8){
+                availableMoves.push(new Square(currentRow, currentCol));
+            }
+
+        }
+
+        return availableMoves;
     }
 }
