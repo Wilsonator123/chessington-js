d169bb302126d48d77d2371e9ec4762b42ed27b8
Wilsonator123 <106590637+Wilsonator123@users.noreply.github.com>
2023-06-30 09:37:52 +0100

    Red-8 Pawn Obstruction

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,26 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const directions = [
+            {dr: 1, dc: 0}, // Up
+            {dr: -1, dc: 0}, // Down
+            {dr: 0, dc: -1}, // Left
+            {dr: 0, dc: 1}, // Right
+            {dr: -1, dc: -1}, // Up-left
+            {dr: -1, dc: 1},  // Up-right
+            {dr: 1, dc: -1},  // Down-left
+            {dr: 1, dc: 1}    // Down-right
+        ];
+        let availableMoves = [];
+        let currentSquare = board.findPiece(this);
+
+        for (let direction of directions) {
+            let currentRow = currentSquare.row;
+            let currentCol = currentSquare.col;
+            availableMoves.push()
+        }
+
+        return availableMoves;
+
     }
 }
